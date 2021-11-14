6505a5a29ba0d6511c77a3d47103d386c5d147af
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-11-15 04:41:10 +0530

    final app done - 01
    1st app -> removes all the imported list when clicked to 'Clear All' btn

--- 01-lists/src/App.js
+++ 01-lists/src/App.js
@@ -1,10 +1,20 @@
-// import logo from './logo.svg';
+import data from "./data";
+import List from "./List";
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <div className="App">
-      <h1>Learn Lists in React JS</h1>
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          <List people={people} />
+          <button onClick={() => setPeople([])}>Clear All</button>
+        </section>
+      </main>
     </div>
   );
 }
