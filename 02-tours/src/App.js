0b2051b9ba389613bef64d8fbd89977d6d918262
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-11-16 23:21:50 +0530

    02-initial commit

---
+++ 02-tours/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Learn how to delete a single element form a list and refresh it back.</h2>
+    </div>
+  );
+}
+
+export default App;
