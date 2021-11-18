009ac6e93ea8fee43d3f4c2c37beebfb2752134e
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-11-19 02:03:30 +0530

    add: output prev and next id

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -7,6 +7,34 @@ const Review = () => {
     const [index, setIndex] = useState(0);
     const {name, job, image, text} = people[index];
 
+    const checkNumber = (number) => {
+        if(number > people.length -1) {
+            return 0;
+        } else if(number < 0) {
+            return people.length -1;
+        }
+        return number;
+    };
+
+    const nextPerson = () => {
+        setIndex((index)=> {
+            let newIndex = index+1;
+            // if(newIndex > 4) {
+            //     newIndex = 0;
+            // }
+            return checkNumber(newIndex);
+        }) 
+    }
+    const prevPerson = () => {
+        setIndex((index)=> {
+            let newIndex = index-1;
+            // if(newIndex < 0) {
+            //     newIndex = 4;
+            // }
+            return checkNumber(newIndex);
+        })
+    }
+
     return <article className="review">
         <div className="img-container">
             <img src={image} alt={name} className="person-img" />
@@ -20,11 +48,11 @@ const Review = () => {
         <p className="info">{text}</p>
 
         <div className="button-container">
-            <button className="prev-btn" >
+            <button className="prev-btn" onClick={prevPerson}>
                 <FaChevronLeft />
             </button>
 
-            <button className="next-btn" >
+            <button className="next-btn" onClick={nextPerson}>
                 <FaChevronRight />
             </button>
         </div>
