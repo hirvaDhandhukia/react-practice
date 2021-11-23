b11359afe43b7e504147ed3032cdfec107156d63
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-11-24 00:15:29 +0530

    add: iterated through the data array and printed it

--- 04-toggle/src/App.js
+++ 04-toggle/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
-    <div className="App">
-      <h1>Hello Toggle World!</h1>
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {
+            // we mapped through the data array here so that we can iterate throgh them and print the output of questions
+            questions.map((question) => {
+              return <SingleQuestion key={question.id} {...question} />
+            })
+          }
+        </section>
+      </div>
+    </main>
   );
 }
 
