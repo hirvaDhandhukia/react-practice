eb72af51fda26f596fff2e2f3bff4cac2fd0001a
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-02 16:33:57 +0530

    add: html

--- 06-generate-text/src/App.js
+++ 06-generate-text/src/App.js
@@ -1,9 +1,23 @@
+import React, {useState} from 'react';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <h1>Text Generatinator</h1>
+
+    {/* input */}
+      <div>
+        <p>Paragraphs:</p>
+        <input type="number" />
+        <button>GENERATE</button>
+      </div>
+
+    {/* output */}
+      <div>
+
+      </div>
     </div>
   );
 }
