f34eee787a2716ffb2027f44944d9c5c40d5c7a4
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-07 15:35:14 +0530

    add: header with dark, light theme * used bootstrap

--- 07-generate-portfolio/src/components/App.js
+++ 07-generate-portfolio/src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Header from "./Bootstrap/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    Dark: false
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header dark={this.state.Dark}>Portfolio Generatinator</Header>
+        {/* .container>.row>.col-12.text-center */}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              Hi
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ 07-generate-portfolio/src/components/Bootstrap/Header.js
@@ -0,0 +1,22 @@
+const Header = ({ dark, children, className, to, Link }) => {
+    // conditional statement, whichever value will be initially given true will be displayed
+    dark = !!dark ? "dark" : "light";
+    return (
+      <nav
+        className={
+            // the `{dark}` here is variable passed by the conditional statement from line 3
+          `navbar navbar-${dark} bg-${dark}` + (className ? " " + className : "")
+        }
+      >
+        {to ? (
+          <Link to={to} className="navbar-brand">
+            {children}
+          </Link>
+        ) : (
+          <span className="navbar-brand">{children}</span>
+        )}
+      </nav>
+    );
+  };
+  
+  export default Header;
