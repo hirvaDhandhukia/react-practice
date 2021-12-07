bfe1f1610f917596312bfa73b634525560eaf29b
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-07 16:39:32 +0530

    add: btn to toggle dark and light mode

--- 07-generate-portfolio/src/components/App.js
+++ 07-generate-portfolio/src/components/App.js
@@ -5,10 +5,42 @@ class App extends Component {
   state = {
     Dark: false
   };
+
+  // toggle to dark and light modes
+  toggleHeader = () => {
+    this.setState({
+      Dark: !this.state.Dark
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Header dark={this.state.Dark}>Portfolio Generatinator</Header>
+        <Header dark={this.state.Dark} className="mb-4">Portfolio Generatinator</Header>
+        
+        {/* .container>.row>.col-12.text-center */}
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              {/* <button 
+                className={`btn btn${
+                  this.state.Dark ? "-outline" : ""
+                }-primary`}
+                onClick={this.toggleHeader}
+                >
+                  Toggle Dark Mode
+              </button> */}
+              <button
+            className="btn btn-sm btn-outline-primary rounded-circle"
+            onClick={this.toggleHeader}
+          >
+            <i
+              className={"fa fa-" + (this.state.Dark ? "sun" : "moon") + "-o"}
+            ></i>
+          </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
