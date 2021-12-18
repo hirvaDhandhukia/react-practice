582af9c30e994fc8a11b9486dc1916c8b75ac522
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-18 15:09:00 +0530

    add: completed working of input and output

--- 06-generate-text/src/App.js
+++ 06-generate-text/src/App.js
@@ -1,25 +1,44 @@
 import React, {useState} from 'react';
 import './App.css';
+import text from './data';
 import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(count <= 0) {
+      amount = 1;
+    } else if(count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0,amount));
+  };
+
   return (
     <div className="App">
       <h1>Text Generatinator</h1>
 
-    {/* input */}
-      <div>
-        <form action="">
+      <form onSubmit={submitHandler}>
         <label>Paragraphs:</label>
-        <input type="number"/>
+        <input 
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
         <button>GENERATE</button>
       </form>
-      </div>
 
-    {/* output */}
-      <div>
-
-      </div>
+      <article className='output-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </div>
   );
 }
