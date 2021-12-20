0be6c1ef0ff1703c878841908bc24a5c170f6b8d
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-20 18:40:54 +0530

    explain: class vs functional components in react

--- 07-generate-portfolio/src/components/Form.js
+++ 07-generate-portfolio/src/components/Form.js
@@ -1,3 +1,4 @@
+// functional component
 const Form = () => {
     return (
         <div className="Form">
@@ -7,3 +8,11 @@ const Form = () => {
 }
 
 export default Form;
+
+
+//  Difference btwn class and funcitional components in react:- 
+// let there be 2 components C1 & C2, 
+// let's say the C2 wants the input from outside sources(from C1) through props. This makes C2 as a functional component
+// if C1 wants to store some data inside itself, then we use class components
+// if you are using some life cycle methods like ajax loading or something, then you can use classcomponent also, !! by using hooks, you can use ajax etc in the functional components also
+// so, using hooks, we can completely depend our web app on functional components
