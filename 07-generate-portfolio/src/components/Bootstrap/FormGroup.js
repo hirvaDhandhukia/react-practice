e4128751d502d795e551e5620559ef960b9258bf
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-12-21 12:19:02 +0530

    add: form group component

---
+++ 07-generate-portfolio/src/components/Bootstrap/FormGroup.js
@@ -0,0 +1,40 @@
+// coppied from bootstrap
+// to generate inputs and giving them ids and passing variables to trace input of the user.
+const FormGroup = ({
+    Id,
+    Label,
+    Type,
+    onChange,
+    Value,
+    defaultValue,
+    Checked,
+    defaultChecked,
+    Placeholder,
+    Desc
+  }) => {
+    return (
+      <div className="form-group">
+        <label htmlFor={Id}>{Label}</label>
+        <input
+          type={Type}
+          className="form-control"
+          id={Id}
+          name={Id}
+          onChange={onChange}
+          value={Value}
+          placeholder={Placeholder}
+          defaultValue={defaultValue}
+          checked={Checked}
+          defaultChecked={defaultChecked}
+          aria-describedby={Id + "Help"}
+        />
+        {Desc && (
+          <small id={Id + "Help"} className="form-text text-muted">
+            {Desc}
+          </small>
+        )}
+      </div>
+    );
+  };
+  
+  export default FormGroup;
